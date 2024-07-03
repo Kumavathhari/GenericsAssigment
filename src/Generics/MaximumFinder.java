18f1d145d6eeedfba90e281da3f675bad4ce0f18
Hari Naik <kumavathharinaik@gmail.com>
2024-07-03 12:20:49 +0530

    Strings find the maximum

--- src/Generics/MaximumFinder.java
+++ src/Generics/MaximumFinder.java
@@ -1,26 +1,28 @@
 package Generics;
 
 public class MaximumFinder {
+        public static void main(String[] args) {
 
-    public static float findMax(float a, float b, float c) {
-        float max = a;
-        if (b > max) {
-            max = b;
+            String result1 = findMaximum("Apple", "Peach", "Banana");
+            System.out.println("Test Case 1 Result: " + result1);
+
+
+            String result2 = findMaximum("Zebra", "Lion", "Elephant");
+            System.out.println("Test Case 2 Result: " + result2);
+
+
+            String result3 = findMaximum("Cat", "Dog", "Bear");
+            System.out.println("Test Case 3 Result: " + result3);
         }
-        if (c > max) {
-            max = c;
+
+        public static String findMaximum(String a, String b, String c) {
+            String max = a;
+            if (b.compareTo(max) > 0) {
+                max = b;
+            }
+            if (c.compareTo(max) > 0) {
+                max = c;
+            }
+            return max;
         }
-        return max;
     }
-
-    public static void main(String[] args) {
-        float a1 = 3.5f, b1 = 7.2f, c1 = 5.1f;
-        System.out.println("Test Case 1: " + findMax(a1, b1, c1));
-
-        float a2 = -1.0f, b2 = -5.4f, c2 = -3.3f;
-        System.out.println("Test Case 2: " + findMax(a2, b2, c2));
-
-        float a3 = 0.0f, b3 = 0.0f, c3 = 0.0f;
-        System.out.println("Test Case 3: " + findMax(a3, b3, c3));
-    }
-}
